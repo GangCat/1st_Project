955c597d8004f8d59f973149f1bd63f19780eeee
GangCat <windy9300@gmail.com>
2023-09-01 11:38:30 +0900

    PickPosDisplay

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -19,10 +19,25 @@ public class InputManager : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             pickPos = Vector3.zero;
-            if (Functions.Picking("StageFloor", 1<<LayerMask.NameToLayer("StageFloor"), ref pickPos))
+            if (Functions.Picking("StageFloor", 1 << LayerMask.NameToLayer("StageFloor"), ref pickPos))
+            {
+                GameObject pickPosDisplayGo = Instantiate(pickPosPrefab, pickPos, Quaternion.identity, transform);
+                StartCoroutine("DestroypickPosDisplay", pickPosDisplayGo);
                 pickingCallback?.Invoke(pickPos);
+            }
         }
     }
+
+    private IEnumerator DestroypickPosDisplay(GameObject _go)
+    {
+        yield return new WaitForSeconds(pickPosDisplayHideDelay);
+        Destroy(_go);
+    }
+
+    [SerializeField]
+    private GameObject pickPosPrefab = null;
+    [SerializeField]
+    private float pickPosDisplayHideDelay = 0.3f;
 
     private VoidVec3Delegate pickingCallback = null;
     private Vector3 pickPos = Vector3.zero;
