61a930ca8e254efd11971ab77f9c21a4311a947c
GangCat <windy9300@gmail.com>
2023-09-02 23:21:39 +0900

    Optimize Code

--- Assets/Scripts/Manager/SelectableObjectManager.cs
+++ Assets/Scripts/Manager/SelectableObjectManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SelectableObjectManager : MonoBehaviour
 {
     public bool IsListEmpty => listSelectedObject.Count < 1;
-    public bool IsFriendlyUnit => isFriendlyUnit;
+    public bool IsFriendlyUnit => isFriendlyUnitInList;
 
     public void Init()
     {
@@ -29,40 +29,40 @@ public class SelectableObjectManager : MonoBehaviour
         if (tempListSelectableObject.Count < 1) return tempListSelectableObject.ToArray();
 
         SelectableObject tempObj = null;
-        isFriendlyUnit = false;
+        isFriendlyUnitInList = false;
         // 오브젝트를 하나하나 검사.
         foreach (SelectableObject obj in tempListSelectableObject)
         {
             switch (obj.ObjectType)
             {
                 case ESelectableObjectType.FriendlyUnit:
-                    if (!isFriendlyUnit)
+                    if (!isFriendlyUnitInList)
                     {
                         listSelectedObject.Clear();
                         listSelectedObject.Add(obj);
-                        isFriendlyUnit = true;
+                        isFriendlyUnitInList = true;
                     }
                     else
                         listSelectedObject.Add(obj);
                     break;
                 case ESelectableObjectType.FriendlyStructure:
-                    if (isFriendlyUnit) break;
+                    if (isFriendlyUnitInList) break;
                     if (!tempObj)       tempObj = obj;
                     if (!tempObj.ObjectType.Equals(obj.ObjectType))
                         tempObj = obj;
                     break;
                 case ESelectableObjectType.EnemyUnit:
-                    if (isFriendlyUnit) break;
+                    if (isFriendlyUnitInList) break;
                     if (!tempObj)       tempObj = obj;
                     break;
                 case ESelectableObjectType.EnemyStructure:
-                    if (isFriendlyUnit) break;
+                    if (isFriendlyUnitInList) break;
                     if (!tempObj)       tempObj = obj;
                     break;
             }
         }
 
-        if (!isFriendlyUnit)
+        if (!isFriendlyUnitInList)
             listSelectedObject.Add(tempObj);
 
         tempListSelectableObject.Clear();
@@ -72,21 +72,25 @@ public class SelectableObjectManager : MonoBehaviour
 
     public void MoveUnitByPicking(Vector3 _movePos)
     {
-        int numForCalcPos = listSelectedObject.Count - 1;
+        int numForCalcStartPos = listSelectedObject.Count - 1;
         float halfOffset = offset * 0.5f;
-        float startXPos = _movePos.x - numForCalcPos % 4 * halfOffset;
-        float startZPos = _movePos.z + numForCalcPos / 4 * halfOffset;
+        float startXPos = _movePos.x - numForCalcStartPos % 4 * halfOffset;
+        float startZPos = _movePos.z + numForCalcStartPos / 4 * halfOffset;
 
         // 이동시 해당 도착점을 중심점으로 각 오브젝트들의 위치를 결정함. 이 때 버튼만든것처럼 오프셋으로 해서 3by4로
         // 반드시 열 개수는 4개로 즉 반드시 /4랑 %4로 위치 결정
         // /랑 % 이용해서 편하게 ㅇㅇ
-        for(int i = 0; i < numForCalcPos + 1; ++i)
-            listSelectedObject[i].MoveToTargetPos(new Vector3(startXPos + ((i % 4) * offset) ,0f, startZPos - ((i / 4) * offset)));
+        for(int i = 0; i < numForCalcStartPos + 1; ++i)
+            listSelectedObject[i].MoveToTargetPos(
+                new Vector3(
+                    startXPos + ((i % 4) * offset),
+                    0f, 
+                    startZPos - ((i / 4) * offset)));
 
         // 단체 이동
     }
     
-    private bool isFriendlyUnit = false;
+    private bool isFriendlyUnitInList = false;
 
     private List<SelectableObject> tempListSelectableObject = new List<SelectableObject>();
     private List<SelectableObject> listSelectedObject = new List<SelectableObject>();
