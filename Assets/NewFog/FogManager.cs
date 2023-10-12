9615332d2cff2ce7a8ea22f7cdd57f4b383e7bea
GangCat <windy9300@gmail.com>
2023-10-12 12:56:39 +0900

    1step

--- Assets/NewFog/FogManager.cs
+++ Assets/NewFog/FogManager.cs
@@ -8,44 +8,70 @@ public class FogManager : MonoBehaviour
 {
     private void Start()
     {
-        //curFogTexture = GenerateTexture(fogRenderTexture);
+        curFogTexture = GenerateTexture(fogRenderTexture);
+        backBufftexture = GenerateTexture(fogRenderTexture);
 
         Shader.SetGlobalVectorArray("_UnitPositions", myArray);
-        StartCoroutine("UpdateFogCoroutine");
 
 
+        newFogRenderTexture = new RenderTexture(fogRenderTexture.width, fogRenderTexture.height, 0, RenderTextureFormat.ARGB32);
+        newFogRenderTexture.enableRandomWrite = true;
+        newFogRenderTexture.Create();
+        newBackBuffRenderTexture = new RenderTexture(fogRenderTexture.width, fogRenderTexture.height, 0, RenderTextureFormat.ARGB32);
+        newBackBuffRenderTexture.enableRandomWrite = true;
+        newBackBuffRenderTexture.Create();
+
+        fogComputeShader.SetTexture(0, "fogRenderTexture", newFogRenderTexture);
+        fogComputeShader.SetTexture(0, "backBuffRenderTexture", newBackBuffRenderTexture);
+        
+        UpdateFogTexture();
 
 
-        //backBuffRenderTexture = RenderTexture.active;
-        //curFogTexture = GenerateTexture(backBuffRenderTexture);
+        // fogRenderTexture 속성을 fogComputeShader에 전달
 
-        //UpdateProjectorMaterial();
-        //UpdateFog();
+        //UpdateFogTexture();
     }
 
-    private IEnumerator UpdateFogCoroutine()
+    private void UpdateFogTexture()
     {
-        while (true)
-        {
-            fogGenMaterial.SetTexture("_MainTex", tex);
-            yield return null;
-            Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
-            sprite.name = "P";
-            fogImage.sprite = sprite;
-            yield return new WaitForSeconds(updateFogDelay);
-        }
+        Graphics.CopyTexture(fogRenderTexture, newFogRenderTexture);
+        int threadGroupsX = Mathf.CeilToInt(newFogRenderTexture.width / 8f);
+        int threadGroupsY = Mathf.CeilToInt(newFogRenderTexture.height / 8f);
+        fogComputeShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        //Graphics.Blit(fogRenderTexture, backBuffRenderTexture);
+
+        Graphics.CopyTexture(newFogRenderTexture, curFogTexture);
+        Graphics.CopyTexture(newBackBuffRenderTexture, backBufftexture);
+
+        Sprite spriteFog = Sprite.Create(curFogTexture, new Rect(0, 0, curFogTexture.width, curFogTexture.height), new Vector2(0.5f, 0.5f));
+        spriteFog.name = "Fog";
+        fogImage.sprite = spriteFog;
+
+        Sprite spriteBuffer = Sprite.Create(backBufftexture, new Rect(0, 0, backBufftexture.width, backBufftexture.height), new Vector2(0.5f, 0.5f));
+        spriteBuffer.name = "Buffer";
+        bufferImage.sprite = spriteBuffer;
+
+
+
+        //Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        //sprite.name = "P";
+        //fogImage.sprite = sprite;
+
+        Invoke("UpdateFogTexture", updateFogDelay);
     }
 
-    private void UpdateFog()
-    {
-        fogGenMaterial.SetTexture("_MainTex", tex);
-        Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
-        sprite.name = "P";
-        fogImage.sprite = sprite;
-
-        Invoke("UpdateFog", updateFogDelay);
-    }
-
+    //private IEnumerator UpdateFogCoroutine()
+    //{
+    //    while (true)
+    //    {
+    //        fogGenMaterial.SetTexture("_MainTex", tex);
+    //        yield return null;
+    //        Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+    //        sprite.name = "P";
+    //        fogImage.sprite = sprite;
+    //        yield return new WaitForSeconds(updateFogDelay);
+    //    }
+    //}
 
     private Texture2D GenerateTexture(RenderTexture _texture)
     {
@@ -56,11 +82,6 @@ public class FogManager : MonoBehaviour
         newTexture.Apply();
         RenderTexture.active = null;
         return newTexture;
-    }
-
-    private void UpdateProjectorMaterial()
-    {
-
     }
 
     [SerializeField]
@@ -74,11 +95,17 @@ public class FogManager : MonoBehaviour
     [SerializeField]
     private Image fogImage = null;
     [SerializeField]
+    private Image bufferImage = null;
+    [SerializeField]
     private Vector4[] myArray = null;
     [SerializeField]
     private Texture2D tex = null;
+    [SerializeField]
+    private ComputeShader fogComputeShader;
 
     private Texture2D curFogTexture = null;
     private Texture2D backBufftexture = null;
 
+    private RenderTexture newFogRenderTexture = null;
+    private RenderTexture newBackBuffRenderTexture = null;
 }
